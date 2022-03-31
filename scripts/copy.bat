9a5e2fdc204dbfd9a95d3812ce8566d08137bd1b
VaxPex <97077459+VaxPex@users.noreply.github.com>
2022-03-31 22:11:24 +0300

    Update copy.bat

--- scripts/copy.bat
+++ scripts/copy.bat
@@ -1,6 +1,8 @@
-@ECHO OFF
+@echo off
+
 if exist C:\Users\VaxPex\Downloads\Server\plugins\WoolWars (
     rmdir /S /Q C:\Users\VaxPex\Downloads\Server\plugins\WoolWars
 )
+
 mkdir C:\Users\VaxPex\Downloads\Server\plugins\WoolWars
-xcopy C:\Users\VaxPex\Downloads\plugins\WoolWars C:\Users\VaxPex\Downloads\Server\plugins\WoolWars /E /H /C /I
+Xcopy C:\Users\VaxPex\Downloads\plugins\WoolWars C:\Users\VaxPex\Downloads\Server\plugins\WoolWars /E /H /C /I
